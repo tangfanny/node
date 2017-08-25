0f006249cb4768f82b226a89f03575d6dcad942e
Angelia-tt <tangfanny@sina.cn>
2017-08-25 23:39:36 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 var express = require('express');
 var app  = express();
 var routes = require('./routres/index');
+var mongoose = require('mongoose');
 
 app.use(express.static(__dirname+'/public');
 app.set('view engine','html');
 app.engine('html',require('ejs').__express);
 app.set('views',__dirname+'/views');
 
-
+global.dbHandel = require('./database/dbHandel');
+global.db = mongoose.connect('mongodb://localhost:27107/school');
 
 
 app.get('/',function(req,res){
