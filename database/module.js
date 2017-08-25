7c106cee25ff9c6b7eb5367dd441149969d027c8
Angelia-tt <tangfanny@sina.cn>
2017-08-25 23:41:27 +0800

    Update module.js

--- database/module.js
+++ database/module.js
@@ -1,10 +1,25 @@
+var express = require('express');
+var router = express.Router();
 
-module.exports = {
 
-	admin:{
-		name : {type : String},
-		age : {type : Number,default:0},
-		city : {type : String,default:'北京'}
-	}
+router.route('/login').get(function(req,res){
+  res.render('login',{
+    title : 'login'
+   })
+}).post(function(req,res){
+	var name = req.body.name;
+    var adminModel = global.dbHandel.getModel('admin');
 
-}
+    adminModel.find({name : name},function(err,docs){
+    	if(docs){
+    		res.send('ok')
+    	}else{
+    		res.send('no')
+    	}
+    })
+
+    
+  
+});
+
+module.exports = router;
